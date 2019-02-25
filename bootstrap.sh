7bb7446938c56592ae5c119644f555867f8fe012
Lisa Jones <jones.lisa.94@gmail.com>
2019-02-25 14:30:50 -0500

    Implemented host, webserver scenario; started bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# create Docker network
+docker network create foo
+
+# build Docker webserver image
+docker build . -f Dockerfile.webserver --tag=ws
+
+# run Docker webserver container
+docker run --name server --network foo -d ws 
+
+# build Docker host image
+docker build . -f Dockerfile.host --tag=h
+
+# run Docker host with curl command
+docker run --name host --network foo -it h server:8080
+
