48618025d2a8e9cd2f46e5e1d7c7ad5182baee17
Brandon Overall <boverall3@gatech.edu>
2019-02-28 12:10:32 -0500

    Added work in progress for bgp/containernet demo script.  Both hosts currently webserver, ip scheme not set up properly.

---
+++ scripts/bgp_host_webserver.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+from mininet.topo import Topo
+from mininet.net import Containernet
+from mininet.log import lg, info, setLogLevel
+from mininet.util import dumpNodeConnections, quietRun, moveIntf
+from mininet.cli import CLI
+from mininet.node import Switch, OVSKernelSwitch
+
+from subprocess import Popen, PIPE, check_output
+from time import sleep, time
+from multiprocessing import Process
+from argparse import ArgumentParser
+
+import sys
+import os
+import termcolor as T
+import time
+
+setLogLevel('info')
+
+parser = ArgumentParser("Configure simple BGP network in Mininet.")
+parser.add_argument('--sleep', default=3, type=int)
+args = parser.parse_args()
+
+
+def log(s, col="green"):
+    print T.colored(s, col)
+
+
+class Router(Switch):
+    """Defines a new router that is inside a network namespace so that the
+    individual routing entries don't collide.
+
+    """
+    ID = 0
+    def __init__(self, name, **kwargs):
+        kwargs['inNamespace'] = True
+        Switch.__init__(self, name, **kwargs)
+        Router.ID += 1
+        self.switch_id = Router.ID
+
+    @staticmethod
+    def setup():
+        return
+
+    def start(self, controllers):
+        pass
+
+    def stop(self):
+        self.deleteIntfs()
+
+    def log(self, s, col="magenta"):
+        print T.colored(s, col)
+
+
+class SimpleTopo(Topo):
+    """The Autonomous System topology is a simple straight-line topology
+    between AS1 -- AS2 -- AS3.  The rogue AS (AS4) connects to AS1 directly.
+
+    """
+    def __init__(self):
+        # Add default members to class.
+        super(SimpleTopo, self ).__init__()
+
+        num_ases = 3
+
+        # The topology has one router per AS
+	routers = []
+        for i in xrange(num_ases):
+            router = self.addSwitch('R%d' % (i+1))
+	    routers.append(router)
+        for i in xrange(num_ases):
+            router = 'R%d' % (i+1)
+
+        for i in xrange(num_ases-1):
+            self.addLink('R%d' % (i+1), 'R%d' % (i+2))
+
+        routers.append(self.addSwitch('R4'))
+       
+        # This MUST be added at the end
+        self.addLink('R1', 'R4')
+        return
+
+def main():
+    os.system("rm -f /tmp/R*.log /tmp/R*.pid logs/*")
+    os.system("mn -c >/dev/null 2>&1")
+    os.system("killall -9 zebra bgpd > /dev/null 2>&1")
+
+    net = Containernet(topo=SimpleTopo(), switch=Router)
+    net.start()
+    for router in net.switches:
+        router.cmd("sysctl -w net.ipv4.ip_forward=1")
+        router.waitOutput()
+
+    log("Waiting %d seconds for sysctl changes to take effect..."
+        % args.sleep)
+    sleep(args.sleep)
+
+    for router in net.switches:
+        router.cmd("/usr/sbin/zebra -f conf/zebra-%s.conf -d -i /tmp/zebra-%s.pid > logs/%s-zebra-stdout 2>&1" % (router.name, router.name, router.name))
+        router.waitOutput()
+        router.cmd("/usr/sbin/bgpd -f conf/bgpd-%s.conf -d -i /tmp/bgp-%s.pid > logs/%s-bgpd-stdout 2>&1" % (router.name, router.name, router.name), shell=True)
+        router.waitOutput()
+        log("Starting zebra and bgpd on %s" % router.name)
+	if router.name == "R1":
+		r1 = router
+	if router.name == "R2":
+		r2 = router
+
+    info('*** Adding docker containers\n')
+    d1 = net.addDocker('d1', path="./Dockerfile.webserver",rm=True)
+    d2 = net.addDocker('d2', path="./Dockerfile.webserver",rm=True)
+    
+    net.addLink(d1,r1)
+    net.addLink(d2,r2)
+
+    CLI(net)
+    net.stop()
+    os.system("killall -9 zebra bgpd")
+
+
+if __name__ == "__main__":
+    main()
